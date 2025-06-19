1e171dff37c5f880c7a4854f7f860e75b443f35c
BhaktiModak <103882747+BhaktiModak@users.noreply.github.com>
2025-06-19 14:09:05 +0530

    Add new files

---
+++ art-gallery.js
@@ -0,0 +1,23 @@
+document.getElementById(culturalArt).addEventListener('click',function(){
+    window.location.href = 'cultural.html'
+});
+
+document.getElementById(popart).addEventListener('click',function(){
+    window.location.href = 'popart.html'
+})
+
+document.getElementById(canvasart).addEventListener('click',function(){
+    window.location.href = 'canvasart.html'
+})
+
+document.getElementById(loveart).addEventListener('click',function(){
+    window.location.href = 'loveart.html'
+})
+
+document.getElementById(wildlifeart).addEventListener('click',function(){
+    window.location.href = 'wildlifeart.html'
+})
+
+document.getElementById(universeart).addEventListener('click',function(){
+    window.location.href = 'universeart.html'
+})
